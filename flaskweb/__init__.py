8cbc5e8fb8e98629c0efe3678f10185d403bed3a
Leno <42096622+xhua167@users.noreply.github.com>
2019-10-13 16:20:49 +1100

    Update __init__.py

--- flaskweb/__init__.py
+++ flaskweb/__init__.py
@@ -9,14 +9,13 @@ jsglue = JSGlue()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4d7e72400c6b35d23f38678282d3ba5d'
 
-app.config['BASIC_AUTH_USERNAME'] = 'admin'
-app.config['BASIC_AUTH_PASSWORD'] = 'help1024'
-app.config['BASIC_AUTH_FORCE'] = True
-basic_auth = BasicAuth(app)
+# app.config['BASIC_AUTH_USERNAME'] = 'admin'
+# app.config['BASIC_AUTH_PASSWORD'] = 'help1024'
+# app.config['BASIC_AUTH_FORCE'] = True
+# basic_auth = BasicAuth(app)
 
 jsglue.init_app(app)
 
-# jsglue = JSGlue(app)
 
 uri = 'mongodb://lenomongo:kzUR8Q9x2b0CSbxp8A0lpVpXPIl07VEnwhCDlrKZLv7imd7Ik0B6O5tpZk6khUemBbDqH8OgdTaAxQxFdtrRdw==@lenomongo.documents.azure.com:10255/?ssl=true&replicaSet=globaldb'
 client = MongoClient(uri)
